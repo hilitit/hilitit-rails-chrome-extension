9654b4fee38b993d23ec40e1fafddc97b313358c
Mohammed O. Tillawy <tillawy@gmail.com>
2015-09-26 08:47:22 +0300

    add login form

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -7,3 +7,23 @@ chrome.runtime.onInstalled.addListener(details => {
 chrome.browserAction.setBadgeText({text: '\'Allo'});
 
 console.log('\'Allo \'Allo! Event Page for Browser Action');
+
+var isLoggedIn = function(){
+  console.log("isLoggedIn()");
+  return false;
+};
+
+
+chrome.runtime.onMessage.addListener(function (request, sender, response) {
+
+  console.log("request");
+  console.log(request);
+  console.log("sender");
+  console.log(sender);
+  console.log("response");
+  console.log(response);
+
+  console.log('chrome message: '  +  request + " "  + sender  + " " + response  + " "  + request.type.startsWith('login') );
+
+
+});
--- app/scripts.babel/options.js
+++ app/scripts.babel/options.js
@@ -1,3 +1,32 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Option');
+
+document.addEventListener('DOMContentLoaded', function () {
+  console.log('\'Allo \'Allo! Option');
+
+  var background = chrome.extension.getBackgroundPage();
+
+  var checkUI = function() {
+    console.log("checkUI");
+
+    if (background.isLoggedIn()){
+      console.log('user is logged in');
+      $('#login').hide();
+      $('#logout').show();
+    } else {
+      console.log('user is logged out');
+      $('#logout').hide();
+      $('#login').show();
+    }
+
+  }
+
+  checkUI();
+  
+  $('#login-form').submit(function(event){
+    console.log("login-form");
+    chrome.runtime.sendMessage({type: this.id});
+    event.preventDefault();
+  });
+
+});
